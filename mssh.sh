c038fbf315aff93eb182875969de5b09d2eaecf4
Puresoul <root@w1.nanox.cz>
2016-10-03 12:38:43 +0200

    multiexec ssh

---
+++ mssh.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Uzivatel pod kterym je klic
+
+USER="user"
+
+# Cesta k klici
+
+KEY=~/.ssh/id_rsa
+
+# Seznam serveru, co radek to server
+
+LIST="s1 s2 s3"
+
+# Overeni ze byl zada prikaz
+
+if [ "$1" == "" ]; then
+    echo "\$0 \"command\" \$1 sudo pass"
+    exit
+else
+
+    for SERVER in $(echo $LIST); do
+	if [ "$1" = "-" ]; then
+	    ssh -t -i $KEY $USER@s$SERVER "echo '#!/bin/bash' > /tmp/cmd; echo \"$2\" >> /tmp/cmd; chmod 777 /tmp/cmd"
+	    ssh -t -i $KEY $USER@s$SERVER "sudo -S /tmp/cmd <<< '$3'"
+	    ssh -t -i $KEY $USER@s$SERVER "rm /tmp/cmd"
+	else
+	    printf "COMMAND FOR SERVER $SERVER:\n\n"
+	    ssh -t -i $KEY $USER@s$SERVER "sudo -S $1 <<< \"$2\""
+	    printf "\n\nDISCONECT FROM $SERVER\n"
+	fi
+	read ANY
+	test ANY && clear
+    done
+
+fi
