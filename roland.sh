07e46115f1c2b9626954b6f9a8617c2132fde0fd
Sajner <no@ma.il>
2017-01-31 18:30:03 +0100

    Comments

--- roland.sh
+++ roland.sh
@@ -27,71 +27,102 @@ fi
 printf "$NEXT"
 }
 
+#
+# Start
+#
 
 if [ "$#" = "0" ]; then
-    echo "\$1 - audio file to convert"
-    echo "\$2 - the number in name of output file"
+    echo "\$1 - Path to directory where samples to convert is located"
+    echo "example: script is at same place with \"samples\" directory"
+    echo " ./roland.sh ./samples"
     exit 0
 fi
+
+#
+# Directory where to find samples
+#
 
 DIR="$1"
 
-NUM=0
+#
+# Temp variable to make output file name
+#
+
+NUM=1
+
+#
+# Main cycle loop
+#
 
 while read FILE; do
 
-let NUM=NUM+1
+    printf "$FILE"
 
-printf "$FILE"
+    #
+    # Check for suffix and set data for header
+    #
 
-if [ "`echo "$FILE" | sed 's/$*.*\\.//'`" = "wav" ]; then
-    SUFIX=".wav"
-    STR="data"
-elif [ "`echo "$FILE" | sed 's/$*.*\\.//'`" = "aif" ]; then
-    STR="SSND"
-    SUFIX=".aif"
-else
-    printf " has Unknown suffix\n"
-    continue
-fi
+    if [ "`echo "$FILE" | sed 's/$*.*\\.//'`" = "wav" ]; then
+	SUFIX=".wav"
+	STR="data"
+    elif [ "`echo "$FILE" | sed 's/$*.*\\.//'`" = "aif" ]; then
+	STR="SSND"
+	SUFIX=".aif"
+    else
+	printf " has Unknown suffix\n"
+	continue
+    fi
 
-if [ "`echo $NUM | wc -c`" = "2" ]; then
-    FILENAME="000${NUM}"
-elif [ "`echo $NUM | wc -c`" = "3" ]; then
-    FILENAME="00${NUM}"
-elif [ "`echo $NUM | wc -c`" = "4" ]; then
-    FILENAME="0${NUM}"
-elif [ "`echo $NUM | wc -c`" = "5" ]; then
-    FILENAME="${NUM}"
-fi
+    #
+    # Make number for output file name
+    #
 
-if [ -f "smpl${FILENAME}.aif" ]; then
-    printf " skiped!\n"
-    exit 0
-else
-    printf " procesing,"
-fi
+    if [ "`echo $NUM | wc -c`" = "2" ]; then
+	FILENAME="000${NUM}"
+    elif [ "`echo $NUM | wc -c`" = "3" ]; then
+	FILENAME="00${NUM}"
+    elif [ "`echo $NUM | wc -c`" = "4" ]; then
+	FILENAME="0${NUM}"
+    elif [ "`echo $NUM | wc -c`" = "5" ]; then
+	FILENAME="${NUM}"
+    fi
 
-NAME="`Head "$(basename "$" $SUFIX)"`"
+    #
+    # When runing over partialy converted set of files, it skips if file exists
+    #
 
-OFF="`Find "$FILE"`"
+    if [ -f "smpl${FILENAME}.aif" ]; then
+	printf " skiped!\n"
+	continue
+    else
+	printf " procesing,"
+    fi
 
-dd if="$FILE" bs=1c of=tmp skip=$OFF 2> /dev/null
+    #
+    # Proces the original filename into new header, than truncate old header
+    #
 
-if [ "$SUFIX" = ".aif" ]; then
+    NAME="`Head "$(basename "$" $SUFIX)"`"
+    OFF="`Find "$FILE"`"
 
-    printf "FORM!ÛäAIFFCOMM!!!!!ì|!@¬D!!!!!!MARK!!!\"!!!!!!beg.loop!!!ì|end.loop!INST!!!<!!!!!!!!!!!!!!APPL!!pRLNDroifxvmc!!!¸$NAME!!!!!!!!!ì|!ì|!!!	!!!!!!!!!!:ê!!uÔ!!°¾!!ë¨!&’!a|!œf!×P!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!D\\\xvfs!! !!!!!	!!!!!!!!!!!!!:ê!!uÔ!!°¾!!ë¨!&’!a|!œf!×P!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!D\!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" | tr '!' '\0' > header
+    dd if="$FILE" bs=1c of=tmp skip=$OFF 2> /dev/null
 
-elif [ "$SUFIX" = ".wav" ]; then
+    if [ "$SUFIX" = ".aif" ]; then
+	printf "FORM!ÛäAIFFCOMM!!!!!ì|!@¬D!!!!!!MARK!!!\"!!!!!!beg.loop!!!ì|end.loop!INST!!!<!!!!!!!!!!!!!!APPL!!pRLNDroifxvmc!!!¸$NAME!!!!!!!!!ì|!ì|!!!	!!!!!!!!!!:ê!!uÔ!!°¾!!ë¨!&’!a|!œf!×P!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!D\\\xvfs!! !!!!!	!!!!!!!!!!!!!:ê!!uÔ!!°¾!!ë¨!&’!a|!œf!×P!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!D\!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" | tr '!' '\0' > header
+    elif [ "$SUFIX" = ".wav" ]; then
+	printf "RIFFˆ¾!!WAVEfmt !!!!!D¬!!±!!!RLNDl!!roifxvmc¸!!!$NAME!!!!!!!!ü.!!ü.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!ø*xvfs !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!ø*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" | tr '!' '\0' > header
+    fi
 
-    printf "RIFFˆ¾!!WAVEfmt !!!!!D¬!!±!!!RLNDl!!roifxvmc¸!!!$NAME!!!!!!!!ü.!!ü.!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!ø*xvfs !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!<!ø*!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" | tr '!' '\0' > header
+    #
+    # Concate new header with truncate file
+    #
 
-else
-    continue
-fi
+    cat header tmp > smpl${FILENAME}.aif
 
-cat header tmp > smpl${FILENAME}.aif
+    printf " Done!\n"
 
-printf " Done!\n"
+    let NUM=NUM+1
 
 done < <(find "$1" -type f)
+
+rm header tmp
