508cc63cf6151adf75a354a5dda703c274aa4e81
root <root@hack.mapfactor.com>
2017-08-22 11:49:46 +0200

    add uniq

---
+++ uniq.go
@@ -0,0 +1,18 @@
+
+// UniqStr returns a copy if the passed slice with only unique string results.
+func UniqStr(col []string) []string {
+	m := map[string]struct{}{}
+	for _, v := range col {
+		if _, ok := m[v]; !ok {
+			m[v] = struct{}{}
+		}
+	}
+	list := make([]string, len(m))
+
+	i := 0
+	for v := range m {
+		list[i] = v
+		i++
+	}
+	return list
+}
